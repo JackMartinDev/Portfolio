50c963ee3ab94833a30b91ebcb661a39f288e600
Jack Martin <jackmartin.dev@gmail.com>
2023-11-27 11:55:43 +0900

    feat: Added link to resume(placeholder)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import GithubActivityList from './components/githubActivity/GithubActivityList'
+import { Anchor } from '@mantine/core'
 
 function App() {
 
     return (
         <>
+            <Anchor href="../public/resume/rakumo.pdf" target="_blank" type="application/pdf" >Resume</Anchor>
             <GithubActivityList user="JackMartinDev"/>
         </>
     )
