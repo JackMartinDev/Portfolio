e1f40a45ebe74da236e81134d206b1ca56eb0b8c
Jack Martin <jackmartin.dev@gmail.com>
2023-12-09 15:40:46 +0900

    feat: Styled contact section

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,13 +1,15 @@
-import { Button, Container, Title } from "@mantine/core"
 import classes from "./Contact.module.css"
+import globalClasses from "../../global.module.css"
 import { useTranslation } from "react-i18next"
+import cx from 'clsx';
 const Contact = ():JSX.Element => {
     const {t} = useTranslation()
     return(
-        <Container id="contact" mb={500} size={800}>
-            <Title className={classes.title}>{t("contact")}</Title>
-            <Button component="a" href="mailto:jackmartin.dev@gmail.com">Get in touch!</Button>
-        </Container>
+        <section className={classes.contact} id="contact">
+            <h3 className={cx(classes.title, globalClasses.sectionTitle)}>{t("contact")}</h3>
+            <p className={classes.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate neque dignissimos voluptatem consequatur sit incidunt facilis excepturi quae aspernatur, quam sint, minima quisquam nihil. Blanditiis quia ducimus unde a reiciendis.</p>
+            <a className={classes.button} href="mailto:jackmartin.dev@gmail.com">Get in touch!</a>
+        </section>
     )
 }
 
