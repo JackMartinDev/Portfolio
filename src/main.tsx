c5aa9d94b480d72f498022152a6db24de7c0ee35
Jack Martin <jackmartin.dev@gmail.com>
2023-12-10 13:27:50 +0900

    feat: Completely removed mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import '@mantine/core/styles.css';
 import './index.css'
-import { MantineProvider, createTheme } from '@mantine/core';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { translationsEn } from "./translations/en"
 import { translationsJp } from "./translations/jp"
-
-const theme = createTheme({})
 
 i18next.use(initReactI18next).init({
     resources:{
@@ -23,10 +19,8 @@ i18next.use(initReactI18next).init({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <MantineProvider theme={theme}>
             <Suspense fallback="Loading...">
                 <App />
             </Suspense>
-        </MantineProvider>
     </React.StrictMode>,
 )
