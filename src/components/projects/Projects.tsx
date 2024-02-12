76cee1658443ebf3f1897e82a42fd56249935dae
Jack Martin <jackmartin.dev@gmail.com>
2024-02-12 15:06:16 +0900

    chore: Moved projects object to its own file

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,15 +2,7 @@ import classes from "./Projects.module.css"
 import { useTranslation } from "react-i18next"
 import cx from "clsx"
 import Project from "./Project"
-
-const projects = [
-    {
-    title: "Project name",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, atque a deserunt porro cumque aperiam incidunt minus nobis itaque in maiores illo ab id tempora voluptas sunt delectus perferendis reprehenderit.",
-    skills: ["Skill 1", "Skill 2", "Skill 3", "Skill 4"],
-    url: "https://github.com/JackMartinDev"
-    },
-]
+import {projects} from "./ProjectsText"
 
 const Projects = ():JSX.Element => {
     const {t} =useTranslation();
---
+++ src/components/projects/ProjectsText.ts
@@ -0,0 +1,8 @@
+export const projects = [
+    {
+    title: "Project name",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, atque a deserunt porro cumque aperiam incidunt minus nobis itaque in maiores illo ab id tempora voluptas sunt delectus perferendis reprehenderit.",
+    skills: ["Skill 1", "Skill 2", "Skill 3", "Skill 4"],
+    url: "https://github.com/JackMartinDev"
+    },
+]
