cc77f44cf489e95747fc7d39724e20d86bbc15ed
Jack Martin <jackmartin.dev@gmail.com>
2024-06-10 13:58:41 +0900

    feat: Adjust git activity styling

--- src/components/githubActivity/Activity.tsx
+++ src/components/githubActivity/Activity.tsx
@@ -6,6 +6,7 @@ import cx from "clsx"
 type Props = {
     activity: TActivity
 }
+
 const Activity = ({activity}:Props):JSX.Element => {
     const [isToggled, setIsToggled] = useState(false);
     const commits = activity.commits.map(commit => (<li>{commit.message}</li>))
@@ -14,17 +15,22 @@ const Activity = ({activity}:Props):JSX.Element => {
         setIsToggled(!isToggled)
     }
 
+    const formatRepo = (repo: string) => {
+        const repoParts = repo.split("/");
+        return repoParts[1];
+    }
+
     return(
         <div className={classes.container}>
             <div className={classes.upper}>
                 <p>
-                    Repo: <a href={activity.URL} className={classes.link} target="_blank">{activity.repo}</a>
+                    Repo: <a href={activity.URL} className={classes.link} target="_blank">{formatRepo(activity.repo)}</a>
                 </p>
                 <p className={classes.time}>{activity.createdAt.toLocaleString()}</p> 
             </div>
 
             <div className={classes.accordion} onClick={toggle}>
-                <h3>Commits: {activity.commits.length}</h3>
+                <h4>Commits: {activity.commits.length}</h4>
                 <span><IconChevronDown size={16} stroke={2} className={cx(classes.icon, {[classes.toggle]: isToggled})}/></span>
             </div>
             <div className={cx(classes.content, {[classes.show]: isToggled})}>
