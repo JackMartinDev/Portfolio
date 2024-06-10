1f4cfb43fc993f0790a120e8e638ad8be18f817e
Jack Martin <jackmartin.dev@gmail.com>
2024-06-10 14:22:27 +0900

    feat: Adjust git history text color

--- src/components/githubActivity/Activity.tsx
+++ src/components/githubActivity/Activity.tsx
@@ -23,14 +23,14 @@ const Activity = ({activity}:Props):JSX.Element => {
     return(
         <div className={classes.container}>
             <div className={classes.upper}>
-                <p>
+                <p className={classes.text}>
                     Repo: <a href={activity.URL} className={classes.link} target="_blank">{formatRepo(activity.repo)}</a>
                 </p>
                 <p className={classes.time}>{activity.createdAt.toLocaleString()}</p> 
             </div>
 
             <div className={classes.accordion} onClick={toggle}>
-                <h4>Commits: {activity.commits.length}</h4>
+                <h4 className={classes.text}>Commits: {activity.commits.length}</h4>
                 <span><IconChevronDown size={16} stroke={2} className={cx(classes.icon, {[classes.toggle]: isToggled})}/></span>
             </div>
             <div className={cx(classes.content, {[classes.show]: isToggled})}>
