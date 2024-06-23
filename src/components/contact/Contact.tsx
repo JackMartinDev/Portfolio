a074d6fb349bade9c87601704b52e3f789626fc2
Jack Martin <jackmartin.dev@gmail.com>
2024-06-23 17:15:50 +0900

    feat: Adjust styling for contact section button and text

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,7 +7,8 @@ const Contact = ():JSX.Element => {
     return(
         <section className={cx(classes.contact, "container")} id="contact">
             <h3 className={cx(classes.title, "sectionTitle")}>{t("contact")}</h3>
-            <p className={classes.text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate neque dignissimos voluptatem consequatur sit incidunt facilis excepturi quae aspernatur, quam sint, minima quisquam nihil. Blanditiis quia ducimus unde a reiciendis.</p>
+            <p className={classes.text}>{t("contact_me_1")}</p>
+            <p className={classes.text}>{t("contact_me_2")}</p>
             <a className={classes.button} href="mailto:jackmartin.dev@gmail.com">Get in touch!</a>
         </section>
     )
