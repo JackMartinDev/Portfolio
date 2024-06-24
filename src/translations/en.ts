23ffd6b78e90080659e653c3813d9bc01705981a
Jack Martin <jackmartin.dev@gmail.com>
2024-06-24 16:33:17 +0900

    feat: Add more translation files

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -5,4 +5,8 @@ export const translationsEn = {
     contact: "Contact",
     resume: "Resume",
     companyLink: "https://www.lanex.co.jp/en",
+
+    //Contact
+    contact_me_1: "I am looking to connect with like-minded developers and grow my professional network. Whether it's about a job opportunity, collaboration on a project, or just to chat about tech, I would love to hear from you.",
+    contact_me_2: "Feel free to reach out to me through my email linked below. I’m always open to interesting conversations and new opportunities.",
 };
